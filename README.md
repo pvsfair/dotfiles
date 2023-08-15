255f301920de5686460e831817ccc755ef5d8bd3
Paulo Alvares <pvsfair@gmail.com>
2023-08-15 16:19:12 +0100

    feat: add packages to `install.sh`
    - jq
    - nvm
    - node

--- README.md
+++ README.md
@@ -6,18 +6,21 @@ The idea of this repo is to auto setup a new distro with all the little things I
 
 In order to install the `install.sh` script has to be called as follow:
 
-```bash
+```bash 
 chmod +x ./install.sh
-. install.sh
+./install.sh
 ```
 
-If not called like this the source of the `~/.bashrc` file will not be called propperly and the changes will only be visible after restarting the terminal, or running `source` command manually
+#### *Deprecated behavior*
+~~*If not called like this the source of the `~/.bashrc` file will not be called propperly and the changes will only be visible after restarting the terminal, or running `source` command manually*~~
+
+After running the `install.sh` script, either restart the terminal or run the command below for the changes in the terminal to take effect
 
 ```bash
 source ~/.bashrc
 ```
 
-# List of apps installed
+# List of apps installed 
 
 - fzf
 - the_fuck
--- install.sh
+++ install.sh
@@ -1,19 +1,88 @@
 #!/usr/bin/env bash
 
-function addstuff(){
-    echo "
-if [ -f \$DOTFILES_PATH/$1 ]; then
-    . \$DOTFILES_PATH/$1
-fi" >> ~/.bashrc
-}
+if [ -z ${DOTFILES_PATH} ]; then
+  echo "setting up bash and aliases"
+  
+  function addstuff(){
+      echo "
+  if [ -f \$DOTFILES_PATH/$1 ]; then
+      . \$DOTFILES_PATH/$1
+  fi" >> ~/.bashrc
+  }
 
-# add this folder path to a var
-echo "export DOTFILES_PATH=$PWD" >> ~/.bashrc
+  # add this folder path to a var
+  echo "export DOTFILES_PATH=$PWD" >> ~/.bashrc
 
-# add source of files on bashrc
+  # add source of files on bashrc
 
-addstuff .bashaliasrc
-addstuff .bashexportsrc
-addstuff .bashrc
+  addstuff .bashaliasrc
+  addstuff .bashexportsrc
+  addstuff .bashrc
 
-source ~/.bashrc
+  # source ~/.bashrc
+  echo "Please Restart you terminal for the changes to take effect. Or you can run `source ~/.bashrc`"
+  
+fi
+if [ -f .env ]; then
+  echo "Reading, now your sensitive data from .env file. (please does not includes the bank acount #, neither you password for fort knox)"
+  set -o allexport
+  source .env set
+  set +o allexport
+  envVarNames=()
+  i=0
+  while read line; do
+    envVarNames[$i]=$(echo $line | cut -d "=" -f 1)
+    i=$i+1
+  done < .env
+
+  filesToSubstituteEnvs=(".wakatime.cfg" ".gitconfig")
+  # create backups and execute for all files
+  for file in ${filesToSubstituteEnvs[@]}; do
+    
+    cp "./$file" "./$file.bkp"  
+    for str in ${envVarNames[@]}; do
+      sed -i "s/\${$str}/${!str}/g" $file
+    done
+    cp "./$file" ~/
+    mv "./$file.bkp" "./$file"
+  done
+else
+  echo "Please copy the .env-default file and input the sensitive data there."
+fi
+
+# installing jq
+if ! command -v jq &> /dev/null; then
+  echo 'Installing jq'
+  sudo apt install jq -y
+fi
+echo "JQ version => $(jq --version)"
+
+if [ -z ${NVM_DIR} ]; then
+  nvmLatestVersion=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r '.tag_name')
+  echo "Installing nvm on version: $nvmLatestVersion"
+
+  curl -sL "https://raw.githubusercontent.com/nvm-sh/nvm/$nvmLatestVersion/install.sh" -o install_nvm.sh
+  bash install_nvm.sh
+  rm install_nvm.sh
+
+  export NVM_DIR="$HOME/.nvm"
+fi
+
+if ! [ -z ${NVM_DIR} ]; then
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+echo "nvm version => $(nvm --version)"
+
+
+if ! [ -z ${NVM_DIR} ]; then
+  if ! command -v node &> /dev/null; then
+    nvm install --lts
+    nodeVersionInstalled=$(nvm --version)
+    echo "Node version $nodeVersionInstalled installed"
+  fi
+fi
+
+echo "Node version => $(node --version)"
+
+echo "bye"
