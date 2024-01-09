9bdf00c039afefc03faa9e927959486ceaecb81d
Paulo Alvares <paulo.alvares@bolttech.io>
2024-01-09 01:06:29 +0000

    feat: add i3 installation

--- install.sh
+++ install.sh
@@ -141,4 +141,20 @@ echo "Node version => $(node --version)"
 
 sh ./install_nvim.sh
 
+if ! command -v i3 &> /dev/null; then
+  echo 'Installing i3'
+  sudo apt install i3 -y
+fi
+
+echo 'Configuring i3 and creating symlink'
+# check if .config/nvim folder exists before creating symlink
+if [ -d ~/.config/i3 ]; then
+  mv ~/.config/i3{,.bkp}
+fi
+
+# check if .config/nvim still exists, if not create symlink
+if [ ! -a ~/.config/i3 ]; then
+  ln -s $DOTFILES_PATH/i3 $HOME/.config/i3
+fi
+
 echo "bye"
